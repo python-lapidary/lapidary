32efd81ff5fea219df7b0e50471b388157ddcff3
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-03 15:06:44 +0200

    ✨ Render operation functions.

--- src/lapis/render/elems/client_class.py
+++ src/lapis/render/elems/client_class.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
-from .operation_function import OperationFunctionModel, get_methods
+from .operation_function import OperationFunctionModel, get_path_op_funcs
+from ..refs import ResolverFunc
 from ...openapi import model as openapi
 
 
@@ -9,7 +10,12 @@ class ClientClass:
     methods: list[OperationFunctionModel]
 
 
-def get_client_class(openapi_model: openapi.OpenApiModel) -> ClientClass:
+def get_client_class(openapi_model: openapi.OpenApiModel, package: str, resolver: ResolverFunc) -> ClientClass:
+    functions = []
+    for url_path, path_item in openapi_model.paths.__root__.items():
+        for op in get_path_op_funcs(path_item, [package, 'paths', url_path], resolver):
+            functions.append(op)
+
     return ClientClass(
-        methods=get_methods(openapi_model),
+        methods=functions
     )
