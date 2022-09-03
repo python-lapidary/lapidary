307e700cde80129632b70a96ad5be7c7df91527d
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-03 16:17:04 +0200

    🐛 Move all imports used in annotations to type-checking section.

--- lapidary-render/src/lapis/render/elems/module.py
+++ lapidary-render/src/lapis/render/elems/module.py
@@ -12,6 +12,8 @@ T = TypeVar('T')
 
 SchemaImport: TypeAlias = tuple[str, str]
 
+template_imports = ['typing']
+
 
 @dataclass(frozen=True)
 class Module:
@@ -25,46 +27,46 @@ class Module:
 def get_schema_class_module(model: openapi.Schema, path: list[str], resolver: ResolverFunc) -> Module:
     schema_class = get_schema_class(model, path, resolver)
 
-    type_checking_imports = {t for attr in schema_class.attributes for t in attr.annotation.type.type_checking_imports()}
-    imports = {t for attr in schema_class.attributes for t in attr.annotation.type.imports()}
-    imports.update(schema_class.base_type.imports())
+    imports = schema_class.base_type.imports()
+    imports.sort()
+
+    type_checking_imports = list({
+        t
+        for attr in schema_class.attributes
+        for t in attr.annotation.type.imports()
+        if t not in imports and t not in template_imports
+    })
+    template_imports.sort()
 
     return Module(
         package='.'.join(path[:-1]),
         name=inflection.underscore(schema_class.class_name),
         body=schema_class,
-        imports=list({
-            'typing',
-            'lapis_client_base',
-            *imports,
-        }),
-        type_checking_imports=list(type_checking_imports),
+        imports=imports,
+        type_checking_imports=type_checking_imports,
     )
 
 
 def get_client_class_module(model: openapi.OpenApiModel, package_name: str, resolver: ResolverFunc) -> Module:
     client_class = get_client_class(model, package_name, resolver)
 
-    imports = list({
+    imports = [
+        'lapis_client_base',
+    ]
+
+    type_checking_imports = list({
         imp
         for attr in client_class.methods
         for t in attr.params
         for imp in t.annotation.type.imports()
+        if imp not in imports and imp not in template_imports
     })
-
-    import_list = [
-        'typing',
-        'pydantic',
-        'deprecation',
-        'lapis_client_base',
-        *imports,
-    ]
-    import_list.sort()
+    type_checking_imports.sort()
 
     return Module(
         package=package_name,
         name='client',
         body=client_class,
-        imports=import_list,
-        type_checking_imports=[],
+        imports=imports,
+        type_checking_imports=type_checking_imports,
     )
