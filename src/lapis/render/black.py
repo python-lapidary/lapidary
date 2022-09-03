091d953b3e00dfa1cb6c77b71d96942ca1da2c9f
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-03 15:01:57 +0200

    ♻️ Reformat generated code before writing to files.

--- src/lapis/render/black.py
+++ src/lapis/render/black.py
@@ -1,23 +1,9 @@
-import re
-from pathlib import Path
-
-from black import WriteBack, Mode, Report, get_sources, DEFAULT_INCLUDES
-from black.concurrency import reformat_many
+from black import Mode, format_file_contents
 
 
-def format_code(root: Path, src: Path) -> None:
-    import click
-    ctx = click.Context(command=click.Command(name=''), obj=dict(root=root))
-
-    report = Report(quiet=True)
-    reformat_many(
-        sources=get_sources(
-            ctx=ctx, src=(str(src),), quiet=False, verbose=False, include=re.compile(DEFAULT_INCLUDES), exclude=None, extend_exclude=None,
-            force_exclude=None, report=report, stdin_filename=None,
-        ),
+def format_code(code: str) -> str:
+    return format_file_contents(
+        src_contents=code,
         fast=False,
-        write_back=WriteBack.YES,
         mode=Mode(),
-        report=report,
-        workers=None,
     )
