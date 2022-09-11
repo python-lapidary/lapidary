79ae7e3efb91fee38d6d9958808ed536e4da79fc
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-11 19:36:02 +0200

    ✨ Handle path and anonymous schemas

--- src/lapis/render/elems/schema_class.py
+++ src/lapis/render/elems/schema_class.py
@@ -1,40 +1,96 @@
-from dataclasses import dataclass
+import logging
+from dataclasses import dataclass, field
+from typing import Optional, Generator
 
-from typing import Optional, Union
+import inflection
 
-from .attribute import AttributeModel, get_attribute, get_enum_attribute
-from .type_ref import TypeRef
+from .attribute import AttributeModel, get_attributes, get_enum_attribute
+from .attribute_annotation import AttributeAnnotationModel
+from .type_ref import TypeRef, BuiltinTypeRef
+from ..module_path import ModulePath
 from ..refs import ResolverFunc
 from ...openapi import model as openapi
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
 class SchemaClass:
     class_name: str
     base_type: TypeRef
-    attributes: list[AttributeModel]
-    docstr: Optional[str]
+    docstr: Optional[str] = None
+    attributes: list[AttributeModel] = field(default_factory=list)
+
+
+def get_schema_classes(
+        schema: openapi.Schema,
+        name: str,
+        module: ModulePath,
+        resolver: ResolverFunc,
+) -> Generator[SchemaClass, None, None]:
+    if schema.enum is not None:
+        yield get_enum_class(schema, name)
+        name = name + 'Value'
+
+    schema_class = get_schema_class(schema, name, module, resolver)
+    if schema_class is not None:
+        yield schema_class
+
+    # handle sub schemas
+
+    if schema.type is openapi.Type.array:
+        item_schema = schema.items
+        if isinstance(item_schema, openapi.Schema):
+            yield from get_schema_classes(item_schema, name + 'Item', module, resolver)
+    elif schema.type is openapi.Type.object:
+        if schema.properties:
+            for prop_name, prop_schema in schema.properties.items():
+                if not isinstance(prop_schema, openapi.Schema):
+                    continue
+                yield from get_schema_classes(prop_schema, name + inflection.camelize(prop_name), module, resolver)
 
 
 def get_schema_class(
-        schema: Union[openapi.Schema, openapi.Reference],
-        path: list[str],
+        schema: openapi.Schema,
+        name: str,
+        module: ModulePath,
         resolver: ResolverFunc,
-) -> SchemaClass:
-    if isinstance(schema, openapi.Reference):
-        schema, path = resolver(schema)
+) -> Optional[SchemaClass]:
+    if schema.type is not openapi.Type.object:
+        return None
 
-    if schema.enum:
-        attributes = [get_enum_attribute(value) for value in schema.enum]
-        base_type = TypeRef.from_str('enum.Enum')
-    else:
-        attributes = [get_attribute(attr, schema.required and attr_name in schema.required, [*path, attr_name], resolver) for attr_name, attr in
-                      schema.properties.items()] if schema.properties else []
-        base_type = TypeRef.from_str('pydantic.BaseModel')
+    logger.info(name)
+
+    base_type = TypeRef.from_str('pydantic.BaseModel')
+    attributes = get_attributes(schema, name, module, resolver) if schema.properties else []
 
     return SchemaClass(
-        class_name=path[-1],
+        class_name=name,
         base_type=base_type,
         attributes=attributes,
         docstr=schema.description or None,
     )
+
+
+def get_enum_class(
+        schema: openapi.Schema,
+        name: str
+):
+    return SchemaClass(
+        class_name=name,
+        base_type=TypeRef.from_str('enum.Enum'),
+        attributes=[get_enum_attribute(v) for v in schema.enum],
+        docstr=schema.description or None,
+    )
+
+
+def get_enum_value(value, schema: openapi.Schema) -> AttributeModel:
+    # if schema.type == openapi.Type.string:
+    if value is None:
+        name = 'none'
+    elif value == '':
+        name = 'empty'
+    else:
+        name = value
+    value = "'" + value.replace("'", r"\'") + "'" if value is not None else None
+    return AttributeModel(name, AttributeAnnotationModel(BuiltinTypeRef(name='__ignored__'), dict(default=value)))
---
+++ src/lapis/render/elems/schema_module.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass, field
+from typing import Union, Optional
+
+import inflection
+
+from .module import AbstractModule, template_imports
+from .param_model_class import get_param_model_classes
+from .schema_class import SchemaClass, get_schema_classes
+from ..module_path import ModulePath
+from ..refs import ResolverFunc
+from ...openapi import model as openapi
+
+
+@dataclass(frozen=True)
+class SchemaModule(AbstractModule):
+    """
+    One schema module per schema element directly under #/components/schemas, containing that schema and all non-reference schemas.
+    One schema module for all schemas under each operation
+
+    """
+    body: list[SchemaClass] = field(default_factory=list)
+
+
+def get_modules_for_components_schemas(
+        schemas: dict[str, Union[openapi.Schema, openapi.Reference]], root_package: ModulePath, resolver: ResolverFunc
+) -> list[SchemaModule]:
+    result = []
+    for name, schema in schemas.items():
+        if isinstance(schema, openapi.Schema):
+            module = get_module_for_components_schema(schema, root_package / inflection.underscore(name), resolver)
+            if module is not None:
+                result.append(module)
+    return result
+
+
+def get_module_for_components_schema(schema: openapi.Schema, path: ModulePath, resolver: ResolverFunc) -> Optional[SchemaModule]:
+    classes = [cls for cls in get_schema_classes(schema, inflection.camelize(path.parts[-1]), path, resolver)]
+    if len(classes):
+        return _get_module_for_components_schema(classes, path)
+
+
+def _get_module_for_components_schema(classes: list[SchemaClass], path: ModulePath) -> SchemaModule:
+    imports = list({
+        imp
+        for cls in classes
+        if cls.base_type is not None
+        for imp in cls.base_type.imports()
+        if imp not in template_imports
+    })
+
+    type_checking_imports = list({
+        t
+        for schema_class in classes
+        for attr in schema_class.attributes
+        for t in attr.annotation.type.imports()
+        if t not in imports and t not in template_imports
+    })
+    type_checking_imports.sort()
+
+    return SchemaModule(
+        path=path,
+        body=classes,
+        imports=imports,
+        type_checking_imports=type_checking_imports,
+    )
+
+
+def get_modules_for_param_model_classes(op: openapi.Operation, module: ModulePath, resolve: ResolverFunc) -> Optional[SchemaModule]:
+    mod = module / 'schemas'
+    classes = [cls for cls in get_param_model_classes(op, op.operationId, mod, resolve)]
+    return _get_module_for_components_schema(classes, mod)
