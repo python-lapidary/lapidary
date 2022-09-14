180082759ae79a30966b3139dc0522179a6ab1c6
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-14 19:01:23 +0200

    🐛 Fix usage of Absent with pydantic

--- lapidary-base/src/lapis_client_base/absent.py
+++ lapidary-base/src/lapis_client_base/absent.py
@@ -1,5 +1,17 @@
 class Absent:
-    pass
+    @classmethod
+    def __get_validators__(cls):
+        yield Absent._validate
+
+    @staticmethod
+    def _validate(obj: 'Absent'):
+        if obj is not ABSENT:
+            raise ValueError()
+        else:
+            return obj
+
+    def __repr__(self):
+        return '-ABSENT-'
 
 
 ABSENT = Absent()
