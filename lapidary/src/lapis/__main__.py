d839577dd87a345860b2b123726e2c1b478ff1e2
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-14 18:57:19 +0200

    ✨ Cache the spec to pickle file

--- lapidary/src/lapis/__main__.py
+++ lapidary/src/lapis/__main__.py
@@ -12,7 +12,7 @@ logger.setLevel(logging.INFO)
 
 
 def main(
-        schema_path: str,
+        schema_path: Path,
         target_directory: Path,
         package_name: str,
 ):
@@ -23,10 +23,19 @@ def main(
     render_client(tree, target_directory, package_name)
 
 
-def load_schema(schema_path):
+def load_schema(schema_path: Path):
+    import pickle
+    cache_path = schema_path.with_suffix('.pickle')
+    if cache_path.exists():
+        with open(cache_path, 'br') as fb:
+            return pickle.load(fb)
+
     with open(schema_path, 'rt') as f:
         import yaml
         doc = yaml.safe_load(f)
+
+    with open(cache_path, 'wb') as fb:
+        pickle.dump(doc, fb)
     return doc
 
 
