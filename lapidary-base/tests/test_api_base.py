6d9cb0aba516b65a58975fc5d1144f7d00402900
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-14 19:36:41 +0200

    ✅ Add a test

---
+++ lapidary-base/tests/test_api_base.py
@@ -0,0 +1,9 @@
+import unittest
+
+from lapis_client_base.api_base import _status_code_matches
+
+
+class ApiBaseTest(unittest.TestCase):
+    def test__status_code_matches(self):
+        matches = [m for m in _status_code_matches('400')]
+        self.assertEqual(['400', '40X', '4XX', 'default'], matches)
