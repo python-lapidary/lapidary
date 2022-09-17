5e3acf595c402ca56b67e2bc2ab3611550b77740
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-09-17 11:20:45 +0200

    ✨ Support simple oneOf schemas

---
+++ lapidary-render/tests/test_type_ref.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+from lapis.openapi import model as openapi
+from lapis.render.elems.refs import get_resolver
+from lapis.render.module_path import ModulePath
+from lapis.render.type_ref import get_type_ref, GenericTypeRef, BuiltinTypeRef
+
+schema_carol = openapi.Schema()
+schema_bob = openapi.Schema(properties={'carol': schema_carol})
+model = openapi.OpenApiModel(
+    openapi='3.0.3',
+    info=openapi.Info(title='', version=''),
+    paths=openapi.Paths(__root__={}),
+    components=openapi.Components(
+        schemas=dict(
+            alice=openapi.Schema(oneOf=[
+                openapi.Schema(type=openapi.Type.string),
+                openapi.Schema(type=openapi.Type.integer),
+            ]),
+            bob=openapi.Schema(oneOf=[
+                openapi.Reference(**{'$ref': '#/components/schemas/carol'}),
+                openapi.Reference(**{'$ref': '#/components/schemas/damian'}),
+            ]),
+            carol=openapi.Schema(type=openapi.Type.string),
+            damian=openapi.Schema(type=openapi.Type.integer),
+        )
+    )
+)
+
+
+class OneOfTypeRefTest(TestCase):
+    def test_get_type_ref(self):
+        self.assertEqual(
+            GenericTypeRef(module='typing', name='Union', args=[
+                BuiltinTypeRef(name='str'),
+                BuiltinTypeRef(name='int'),
+            ]),
+            get_type_ref(
+                model.components.schemas['alice'],
+                ModulePath('mymodule'),
+                'Alice',
+                True,
+                get_resolver(model, 'mypackage')
+            )
+        )
+
+    def test_get_type_ref_references(self):
+        self.assertEqual(
+            GenericTypeRef(module='typing', name='Union', args=[
+                BuiltinTypeRef(name='str'),
+                BuiltinTypeRef(name='int'),
+            ]),
+            get_type_ref(
+                model.components.schemas['bob'],
+                ModulePath('mymodule'),
+                'Bob',
+                True,
+                get_resolver(model, 'mypackage')
+            )
+        )
