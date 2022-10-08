c6d0ff2a7ea9162f038b503d5f8cf01618233b39
Raphael Krupinski <10319569-mattesilver@users.noreply.gitlab.com>
2022-10-09 01:56:28 +0200

    ✨ Support for errata directory; 🐛 Fix handling cache flag.

--- src/lapidary/load.py
+++ src/lapidary/load.py
@@ -10,18 +10,22 @@ from lapidary.config import Config
 logger = logging.getLogger(__name__)
 
 
-def load_as_dict(path: Path, cache_root: Path) -> dict:
+def load_yaml_cached(path: Path, cache_root: Path, use_cache: bool) -> dict:
     with open(path, 'rt') as fb:
         text = fb.read()
-    digest = hashlib.sha224(text.encode()).hexdigest()
-    cache_path = (cache_root / digest).with_suffix('.pickle')
-    if cache_path.exists():
-        with open(cache_path, 'br') as fb:
-            return pickle.load(fb)
+
+    do_cache = use_cache and path.stat().st_size > 50_000
+    if do_cache:
+        digest = hashlib.sha224(text.encode()).hexdigest()
+        cache_path = (cache_root / digest).with_suffix('.pickle')
+        if cache_path.exists():
+            with open(cache_path, 'br') as fb:
+                return pickle.load(fb)
 
     d = yaml.safe_load(text)
-    with open(cache_path, 'bw') as fb:
-        pickle.dump(d, fb)
+    if do_cache:
+        with open(cache_path, 'bw') as fb:
+            pickle.dump(d, fb)
 
     return d
 
@@ -33,11 +37,20 @@ def load_spec(project_root: Path, config: Config) -> dict:
     cache_path.mkdir(exist_ok=True)
 
     logger.info('Load schema')
-    spec_dict = load_as_dict(spec_path, cache_path)
+    spec_dict = load_yaml_cached(spec_path, cache_path, config.cache)
 
     if errata_path is not None:
+        if not errata_path.exists():
+            raise ValueError("'Path doesn't exist", errata_path)
         logger.info('Load errata')
-        errata_dict = load_as_dict(errata_path, cache_path)
+        if errata_path.is_dir():
+            errata_dict = [
+                op
+                for p in errata_path.glob('**/*.yaml')
+                for op in load_yaml_cached(p, cache_path, config.cache)
+            ]
+        else:
+            errata_dict = load_yaml_cached(errata_path, cache_path, config.cache)
         from jsonpatch import JsonPatch
         errata = JsonPatch(errata_dict)
         spec_dict = errata.apply(spec_dict)
