101c82c04409510ded87532d681af3841e009aef
Raphael Krupinski <10319569-mattesilver@users.noreply.gitlab.com>
2022-10-16 23:55:38 +0200

    🐛 Fix file and path handling.

--- src/lapidary/render/client.py
+++ src/lapidary/render/client.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from jinja2 import Environment
@@ -8,9 +9,16 @@ from .render import render
 from ..config import Config
 from ..openapi import model as openapi
 
+logger = logging.getLogger(__name__)
 
-def render_client_module(model: openapi.OpenApiModel, config: Config, gen_root: Path, resolver: ResolverFunc, env: Environment):
+
+def render_client_module(
+        model: openapi.OpenApiModel, config: Config, gen_root: Path, resolver: ResolverFunc, env: Environment
+):
     root_mod = ModulePath(config.package)
     client_module_path = root_mod / 'client.py'
+    file_path = client_module_path.to_path(gen_root)
+    logger.info('Render client module to %s', file_path)
+
     client_class_module = get_client_class_module(model, client_module_path, root_mod, resolver)
-    render(client_class_module, 'client_module.py.jinja2', client_module_path.path.to_path(gen_root), env, config.format)
+    render(client_class_module, 'client_module.py.jinja2', file_path, env, config.format)
