3f3edf638ec710c41be4150a45b34258aa969cc3
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-10-23 00:56:54 +0200

    🥅 Catch errors in render.

--- src/lapidary/render/render.py
+++ src/lapidary/render/render.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from typing import Any
 
@@ -5,17 +6,28 @@ from jinja2 import Environment
 
 from .black import format_code
 
+logger = logging.getLogger(__name__)
+
 
 def render(render_model: Any, source: str, destination: Path, env: Environment, format_: bool) -> None:
     try:
         code = env.get_template(source).render(model=render_model)
+    except Exception:
+        logger.info('Failed to render %s', destination)
+        raise
 
-        if format_:
+    if format_:
+        try:
             code = format_code(code)
+        except Exception:
+            logger.info('Failed to format %s', destination)
+            print(code)
+            raise
 
+    try:
         destination.parent.mkdir(parents=True, exist_ok=True)
         with open(destination, 'wt') as fb:
             fb.write(code)
     except Exception:
-        print(destination)
+        logger.info('Failed to save %s', destination)
         raise
