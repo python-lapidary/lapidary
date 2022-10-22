914dcdc7259a9cc89f79fb2f4a80226cfe14dffd
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-10-23 00:18:49 +0200

    ✨ Add support for HTTP Auth.

--- lapidary-base/src/lapidary_base/security.py
+++ lapidary-base/src/lapidary_base/security.py
@@ -12,9 +12,26 @@ class ApiKeyAuth(httpx.Auth, pydantic.BaseSettings):
     api_key: str
     name: str
     placement: ParamLocation
-    value_prefix: Optional[str] = None
 
     def auth_flow(self, request: httpx.Request) -> PageFlowGenT:
-        value = (self.value_prefix if self.value_prefix else '') + self.api_key
-        request.headers[self.name] = value
+        value = self.api_key
+        if self.placement is ParamLocation.header:
+            request.headers[self.name] = value
+        elif self.placement is ParamLocation.query:
+            request.url.params[self.name] = value
+        elif self.placement is ParamLocation.cookie:
+            # TODO
+            raise NotImplementedError(ParamLocation.cookie)
+        else:
+            raise ValueError(self.placement)
         yield request
+
+
+class HTTPAuth(ApiKeyAuth):
+    def __init__(self, scheme: str, token: str, bearer_format: Optional[str] = None):
+        value_format = 'Bearer {token}' if scheme.lower() == 'bearer' else bearer_format
+        super().__init__(
+            api_key=value_format.format(token=token),
+            name='Authorization',
+            placement=ParamLocation.header,
+        )
