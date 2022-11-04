9a4cf2718d3be9568ff755c81b4a6006c063e3af
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-11-04 23:10:41 +0100

    ♻️ Make lapidary a namespace package.

--- lapidary-render/src/lapidary/render/render.py
+++ lapidary-render/src/lapidary/render/render.py
@@ -1,10 +1,16 @@
+import concurrent.futures
 import logging
+import os
 from pathlib import Path
 from typing import Any
 
-from jinja2 import Environment
+from jinja2 import Environment, PackageLoader
 
 from .black import format_code
+from .client import render_client_module
+from .elems import get_resolver
+from .schema import render_schema_modules
+from ..openapi import model as openapi
 
 logger = logging.getLogger(__name__)
 
@@ -31,3 +37,33 @@ def render(render_model: Any, source: str, destination: Path, env: Environment, 
     except Exception:
         logger.info('Failed to save %s', destination)
         raise
+
+
+def render_client(model: openapi.OpenApiModel, target: Path, config: Config) -> None:
+    env = Environment(
+        keep_trailing_newline=True,
+        loader=PackageLoader("lapidary.render"),
+    )
+
+    gen_root = target / 'gen'
+    gen_root.mkdir(parents=True, exist_ok=True)
+
+    resolver = get_resolver(model, config.package)
+
+    with (
+        concurrent.futures.ProcessPoolExecutor() as executor
+    ):
+        client_future = executor.submit(render_client_module, model, config, gen_root, resolver, env)
+        schema_futures = render_schema_modules(model, config, gen_root, resolver, env, executor)
+
+        for f in [*schema_futures, client_future]:
+            f.result()
+
+    ensure_init_py(gen_root, config.package)
+    logger.info('Done.')
+
+
+def ensure_init_py(gen_root, package_name):
+    for (dirpath, dirnames, filenames) in os.walk(gen_root / package_name):
+        if '__init__.py' not in filenames:
+            (Path(dirpath) / '__init__.py').touch()
