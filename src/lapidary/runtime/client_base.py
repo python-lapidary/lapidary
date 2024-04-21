a14b49536bb5875a9877d8584d9718720d814a9f
Raphael Krupinski <10319569-mattesilver@users.noreply.gitlab.com>
2024-04-21 22:29:02 +0200

    ✨ Add kwargs to `lapidary_authenticate` method, complain on duplicate arguments.

--- src/lapidary/runtime/client_base.py
+++ src/lapidary/runtime/client_base.py
@@ -97,11 +97,19 @@ class ClientBase(abc.ABC):
         else:
             return None
 
-    def lapidary_authenticate(self, *auths: NamedAuth) -> None:
-        self._auth.update(auths)
+    def lapidary_authenticate(self, *auth_args: NamedAuth, **auth_kwargs: httpx.Auth) -> None:
+        """Register named Auth instances for future use with methods that require authentication."""
+        if auth_args:
+            # make python complain about duplicate names
+            self.lapidary_authenticate(**dict(auth_args), **auth_kwargs)
+
+        self._auth.update(auth_kwargs)
         self._auth_cache.clear()
 
     def lapidary_deauthenticate(self, *sec_names: str) -> None:
+        """Remove reference to a given Auth instance.
+        Calling with no parameters removes all references"""
+
         if sec_names:
             for sec_name in sec_names:
                 del self._auth[sec_name]
