ff4efb3024ede6e56f863b69da3a285f58d3fa92
Raphael Krupinski <10319569-mattesilver@users.noreply.gitlab.com>
2024-07-28 02:02:46 +0200

    🐛 Add missing WebArg annotation to __init__.py .

--- src/lapidary/runtime/__init__.py
+++ src/lapidary/runtime/__init__.py
@@ -14,6 +14,7 @@ __all__ = (
     'Responses',
     'SecurityRequirements',
     'Simple',
+    'StatusCode',
     'delete',
     'get',
     'head',
@@ -23,7 +24,7 @@ __all__ = (
     'trace',
 )
 
-from .annotations import Body, Cookie, Header, Metadata, Path, Query, Response, Responses
+from .annotations import Body, Cookie, Header, Metadata, Path, Query, Response, Responses, StatusCode
 from .client_base import ClientBase
 from .model import ModelBase
 from .model.encode_param import FormExplode, MatrixExplode, Simple
