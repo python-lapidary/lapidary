bce61ccab1b010f2dc23b3d92aeb7ce0a5da510c
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2024-08-27 19:44:16 +0200

    ♻️ Use session_factory in tests.

--- tests/client.py
+++ tests/client.py
@@ -5,5 +5,4 @@ from lapidary.runtime import ClientBase
 
 class ClientTestBase(ClientBase):
     def __init__(self, client: httpx.AsyncClient):
-        super().__init__(base_url='http://example.com')
-        self._client = client
+        super().__init__(session_factory=lambda **_: client)
